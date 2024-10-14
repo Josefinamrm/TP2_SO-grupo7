0562b516ea36595d088e6b2263bed41f848077a3
martuschvartz <mschvartztallone@asusZmartu>
2024-10-14 17:51:59 -0300

    ps function added

--- Userland/SampleCodeModule/test_processes.c
+++ Userland/SampleCodeModule/test_processes.c
@@ -1,16 +1,18 @@
-#include <stdio.h>
-#include "test_util.h"
+#include <user_syscalls.h>
+#include <user_lib.h>
+#include <test_util.h>
 
-enum State_t { RUNNING_t,
-             BLOCKED_t,
-             KILLED_t };
+enum State { RUNNING,
+             BLOCKED,
+             KILLED };
 
 typedef struct P_rq {
   int32_t pid;
-  enum State_t state;
+  enum State state;
 } p_rq;
 
 int64_t test_processes(uint64_t argc, char *argv[]) {
+print("Dentro de test processes\n");
   uint8_t rq;
   uint8_t alive = 0;
   uint8_t action;
@@ -26,64 +28,62 @@ int64_t test_processes(uint64_t argc, char *argv[]) {
   p_rq p_rqs[max_processes];
 
   while (1) {
-    printArray("antes de crear procesos\n");
+    print("Dentro de while(1)\n");
     // Create max_processes processes
     for (rq = 0; rq < max_processes; rq++) {
-      p_rqs[rq].pid = my_create_process((uint64_t)endless_loop, my_getpid(), 1, 1, argvAux);
+      p_rqs[rq].pid = create_process((uint64_t)endless_loop, get_pid(), 0, 1, argvAux);
 
       if (p_rqs[rq].pid == -1) {
-        printArray("test_processes: ERROR creating process\n");
+        print("test_processes: ERROR creating process\n");
         return -1;
       } else {
-        p_rqs[rq].state = RUNNING_t;
+        p_rqs[rq].state = RUNNING;
         alive++;
       }
     }
 
-        printArray("luego de crear procesos\n");
-
+    print("ya cree los mas processes\n");
 
     // Randomly kills, blocks or unblocks processes until every one has been killed
-     while (alive > 0) {
-      printArray("Antes de segundo for los procesos\n");
+    while (alive > 0) {
+      print("empiezo a matar procesos mientras sigan procesos vivos\n");
       for (rq = 0; rq < max_processes; rq++) {
         action = GetUniform(100) % 2;
 
         switch (action) {
           case 0:
-            if (p_rqs[rq].state == RUNNING_t || p_rqs[rq].state == BLOCKED_t) {
-              if (my_kill(p_rqs[rq].pid) == -1) {
-                printArray("test_processes: ERROR killing process\n");
+            if (p_rqs[rq].state == RUNNING || p_rqs[rq].state == BLOCKED) {
+              if (kill(p_rqs[rq].pid) == -1) {
+                print("test_processes: ERROR killing process\n");
                 return -1;
               }
-              p_rqs[rq].state = KILLED_t;
+              p_rqs[rq].state = KILLED;
               alive--;
             }
             break;
 
           case 1:
-            if (p_rqs[rq].state == RUNNING_t) {
-              if (my_block(p_rqs[rq].pid) == -1) {
-                printArray("test_processes: ERROR blocking process\n");
+            if (p_rqs[rq].state == RUNNING) {
+              if (block(p_rqs[rq].pid) == -1) {
+                print("test_processes: ERROR blocking process\n");
                 return -1;
               }
-              p_rqs[rq].state = BLOCKED_t;
+              p_rqs[rq].state = BLOCKED;
             }
             break;
         }
-      } 
+      }
 
       // Randomly unblocks processes
       for (rq = 0; rq < max_processes; rq++)
-        if (p_rqs[rq].state == BLOCKED_t && GetUniform(100) % 2) {
-          if (my_unblock(p_rqs[rq].pid) == -1) {
-            printArray("test_processes: ERROR unblocking process\n");
+        if (p_rqs[rq].state == BLOCKED && GetUniform(100) % 2) {
+          if (unblock(p_rqs[rq].pid) == -1) {
+            print("test_processes: ERROR unblocking process\n");
             return -1;
           }
-          p_rqs[rq].state = RUNNING_t;
+          p_rqs[rq].state = RUNNING;
         }
-        printArray("despues del segundo for\n");
-    } 
-    timer_wait(2);
+    }
+    print("ya los mate todos\n");
   }
 }
