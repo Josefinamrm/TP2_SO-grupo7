f7ef4907c5660165b0a9941635c6fe775995b1cb
martuschvartz <mschvartztallone@asusZmartu>
2024-11-11 18:35:33 -0300

    pvs starting

--- Kernel/drivers/bitmapFont.c
+++ Kernel/drivers/bitmapFont.c
@@ -1,5 +1,7 @@
 // This is a personal academic project. Dear PVS-Studio, please check it.
 // PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+// This is a personal academic project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
 #include <bitmapFont.h>
 
 // Se encuentran los caracteres del rango 32 a 126 inclusive
